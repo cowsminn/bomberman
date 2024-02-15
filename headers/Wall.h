ce8cc20672c1ac6e4fd729d5abdb1d92e3c97e3c
cowsminn <cowsminn@users.noreply.github.com>
2024-02-16 00:43:25 +0200

    coliziuni ecran

--- headers/Wall.h
+++ headers/Wall.h
@@ -9,13 +9,10 @@ class Wall : public Static_Object {
 public:
     Wall();
 
-    // Constructor to set position
     Wall(float x, float y);
 
     ~Wall() override;
 
-    // Function to check collision with player
-    bool checkCollision(const sf::FloatRect &playerBounds) const;
 };
 
 #endif //OOP_WALL_H
